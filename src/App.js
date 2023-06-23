0320d2be0cd730966ae4eab99bfc2b4653c8e3a9
juanhernandezdeveloper <jualherac@gmail.com>
2023-06-23 11:37:47 -0500

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./Reset.css";
 import "./App.css";
 import MainPage from "./components/MainPage/MainPage";
@@ -9,14 +9,12 @@ import WatchList from "./components/WatchList/WatchList";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/details/:id" element={<DetailsPage />} />
-          <Route path="/my-watch-list" element={<WatchList />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
+        <Route path="/my-watch-list" element={<WatchList />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
     </div>
   );
 }
